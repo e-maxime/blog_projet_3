7fc078ad4b7df1b02c7facf0f0d630981a62c750
Maxime <enrietto.maxime@gmail.com>
2018-03-27 09:01:42 +0200

    Une variable enlevé dans l'autoloader

--- app/Autoloader.php
+++ app/Autoloader.php
@@ -12,7 +12,7 @@ class Autoloader
 	{
 		$class_name = str_replace(__NAMESPACE__ . '\\', '', $class_name);
 		$class_name = str_replace('\\', '/', $class_name);
-
+		
 		require($class_name . '.php');
 	}
 }
